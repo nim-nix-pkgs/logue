745f60cb45d662482992602ecc716fdf8a8ec8de
hugosenari <hugosenari@gmail.com>
2022-03-22 00:34:09 -0300

    chore: re-index ../nimpkgs/l/logue

--- master/flake.nix
+++ master/flake.nix
@@ -9,13 +9,14 @@
   
   inputs.src-logue-master.flake = false;
   inputs.src-logue-master.owner = "planety";
-  inputs.src-logue-master.ref   = "refs/heads/master";
+  inputs.src-logue-master.ref   = "master";
   inputs.src-logue-master.repo  = "logue";
   inputs.src-logue-master.type  = "github";
   
   inputs."cligen".owner = "nim-nix-pkgs";
   inputs."cligen".ref   = "master";
   inputs."cligen".repo  = "cligen";
+  inputs."cligen".dir   = "v1_5_22";
   inputs."cligen".type  = "github";
   inputs."cligen".inputs.nixpkgs.follows = "nixpkgs";
   inputs."cligen".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
